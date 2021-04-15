57d12b4b2863147e1bc02cf0ee323ae2eaefdfce
hanan-tuwaiq <hanan.hakami@tuwaiq.edu.sa>
2021-04-15 11:37:50 +0300

    not complete

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace tweet_analyzer
+{
+    class Program
+    {
+        static void analyzer (string tweet)
+        {
+            string hashtags = "";
+            int hashtagsCounter = 0;
+            string users = "";
+            int usersCounter = 0;
+
+            for (int i = 0; i < tweet.Length; i++)
+            {
+                //if(tweet[i] == '#' && tweet[i+1] != ' ')
+                //{
+                //   while(tweet[i] != ' ')
+                //    {
+                //        hashtags += tweet[i + 1];
+                //        i++;
+                //    }
+                //    hashtagsCounter += 1;
+
+                //}
+
+                //if (tweet[i] == '@' && tweet[i + 1] != ' ')
+                //{
+                //    usersCounter += 1;
+
+                //}
+
+                if(tweet[i] != ' ')
+                {
+                    hashtags += tweet[i];
+                } else
+                {
+                    //Console.WriteLine(hashtags);
+                    hashtags = "";
+                }
+                
+            }
+            //Console.WriteLine(hashtagsCounter);
+            //Console.WriteLine(usersCounter);
+        }
+        static void Main(string[] args)
+        {
+            string tweet = "@Rno71: Yo anyone wanna battle @jasmine #AnimalCrossing #ACNH #NintendoSwitch";
+            analyzer(tweet);
+        }
+    }
+}
