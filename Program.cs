bf74174ba2d31d129315388d67009a1b90ec0d3a
hanan-tuwaiq <hanan.hakami@tuwaiq.edu.sa>
2021-04-15 12:01:06 +0300

    tweet analyzer

--- Program.cs
+++ Program.cs
@@ -4,49 +4,39 @@ namespace tweet_analyzer
 {
     class Program
     {
-        static void analyzer (string tweet)
+        static void analyzer(string tweet)
         {
             string hashtags = "";
             int hashtagsCounter = 0;
             string users = "";
             int usersCounter = 0;
 
-            for (int i = 0; i < tweet.Length; i++)
+            int i = 0;
+            while (i < tweet.Length)
             {
-                //if(tweet[i] == '#' && tweet[i+1] != ' ')
-                //{
-                //   while(tweet[i] != ' ')
-                //    {
-                //        hashtags += tweet[i + 1];
-                //        i++;
-                //    }
-                //    hashtagsCounter += 1;
-
-                //}
-
-                //if (tweet[i] == '@' && tweet[i + 1] != ' ')
-                //{
-                //    usersCounter += 1;
-
-                //}
-
-                if(tweet[i] != ' ')
+                if (tweet[i] == '#' && tweet[i + 1] != ' ')
                 {
-                    hashtags += tweet[i];
-                } else
-                {
-                    //Console.WriteLine(hashtags);
-                    hashtags = "";
+                    hashtagsCounter += 1;
                 }
-                
+
+                if (tweet[i] == '@' && tweet[i + 1] != ' ')
+                {
+                    usersCounter += 1;
+                    {
+                        //
+                        i++;
+                    }
+
+                    Console.WriteLine(hashtagsCounter);
+                    Console.WriteLine(usersCounter);
+                }
             }
-            //Console.WriteLine(hashtagsCounter);
-            //Console.WriteLine(usersCounter);
         }
-        static void Main(string[] args)
-        {
-            string tweet = "@Rno71: Yo anyone wanna battle @jasmine #AnimalCrossing #ACNH #NintendoSwitch";
-            analyzer(tweet);
+                static void Main(string[] args)
+                {
+                    string tweet = "@Rno71: Yo anyone wanna battle @jasmine #AnimalCrossing #ACNH #NintendoSwitch";
+                    analyzer(tweet);
+                }
+            }
         }
-    }
-}
+    
